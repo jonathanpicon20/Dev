64e48118e56643863eaf5375d739f8215f664d0b
Jonathan Picon <jonathan.picon20@gmail.com>
2022-09-06 23:42:16 -0500

    Hola

---
+++ FrontEndFreeLancer/assets/js/alerts.js
@@ -0,0 +1,15 @@
+
+
+// alert('Hola Mundo');
+
+// let nombre = prompt('¿Como te llamas?', 'Sin nombre');
+
+// console.log(nombre)
+// console.log('****' + nombre + '****');
+
+
+// const seleccion = confirm('¿estas seguro de borrar esto?');
+// console.log(seleccion)
+
+
+console.log(global)
---
+++ FrontEndFreeLancer/assets/js/app.js
@@ -0,0 +1,21 @@
+
+alert('hola mundo desde app.js');
+
+
+console.log(miNuevoNombre)
+
+let a = 10, 
+    b = 30,
+    c = 'HOLA ',
+    d = 'SPIDERMAN',
+    x = a + b;
+
+const saludo = c + d;
+
+
+
+
+c = 'HOLA DE NUEVO'
+
+let miNuevoNombre = 'Paul'
+
